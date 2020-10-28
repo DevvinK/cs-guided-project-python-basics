c06af6b6bca30848a5480949a6b5e27778dcfb6d
DevvinK <devvink@gmail.com>
2020-10-27 21:34:19 -0600

    completed tasks 1-10 skipping 8. Working on trying to figure this one out.

--- src/demonstration_06.py
+++ src/demonstration_06.py
@@ -15,6 +15,40 @@ Examples:
 - XO("zpzpzpp") ➞ True (Returns True if no x and o)
 - XO("zzoo") ➞ False
 """
-def XO(txt):
-    # Your code here
+# def XO(txt:str) -> bool:
+#     x_count=0
+#     o_count=0
+#     txt = txt.lower()
+#     for char in txt:
+#         if char == "x": x_count += 1
+#         elif char == "o": o_count += 1
+    
+#     if x_count == o_count: return True
+#     else: return False
 
+
+# def XO(txt:str) -> bool:
+#     x_count=0
+#     o_count=0
+#     txt = txt.lower()
+#     for char in txt:
+#         if char == "x": x_count += 1
+#         elif char == "o": o_count += 1
+
+#     # return x_count is equal to o_count as a boolean to the caller
+#     return x_count==o_count
+
+
+def XO(txt:str) -> bool:
+    lower_txt = txt.lower()
+
+    # return the count of lower txt using "x" as a parameter == the count of 
+    # lower txt using "o" as a parameter as a boolean value to the caller
+    return lower_txt.count("x") == lower_txt.count("o")
+
+
+print(XO("ooxx"))
+print(XO("xooxx"))
+print(XO("ooxXm"))
+print(XO("zpzpzpp"))
+print(XO("zzoo"))
